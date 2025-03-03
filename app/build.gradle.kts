57d1d45828dad37cee7c87589578331f9933bfa1
Ritika Shrestha <shrestharitika998@gmail.com>
2025-03-03 17:55:19 +0545

    Updated files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,13 @@ dependencies {
     implementation("com.cloudinary:cloudinary-android:2.1.0")
     implementation("com.squareup.picasso:picasso:2.8")
 
+
+
+    testImplementation("org.mockito:mockito-core:5.6.0")
+    testImplementation("org.mockito:mockito-inline:3.12.4")
+
+
+
+    androidTestImplementation("androidx.test:runner:1.2.0")
+    androidTestImplementation("androidx.test:rules:1.2.0")
 }
